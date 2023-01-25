22c5cce45d9372e1bdfd5dac1b198dcfd8517f06
Tyhler Keith <tkeith1106@gmail.com>
2023-01-24 22:11:04 -0700

    Added in program aspect of Q2a&b

--- A1_Q2/src/Q2.java
+++ A1_Q2/src/Q2.java
@@ -7,7 +7,6 @@ Description : Swap two adjacent elements in a list by adjusting only the links (
                 a.	singly-linked list (10 marks)
                 b.	doubly-linked list (10 marks)
 
-
 Underlying assumptions for implementation of assignment question:
     - Will only be implementing the methods asked for above as it is assumed any other methods are known via reading
     text and watching YouTube lectures and do not need to be shown, unless necessary to properly implement things.
@@ -19,9 +18,9 @@ Underlying assumptions for implementation of assignment question:
 public class Q2 {
     public static void main(String[] args) {
 
-        // call SLL demo
-//        SLLDemo();
-        // call DLL demo
+        // call SLL demo Q2a
+        SLLDemo();
+        // call DLL demo Q2b
         DLLDemo();
 
     }
@@ -30,7 +29,8 @@ public class Q2 {
         // init queue object
         SLLQueue testQ = new SLLQueue();
 
-        System.out.println("Creating test queue with 8 node of data.\n");
+        System.out.println("SLL Demo:");
+        System.out.println("Creating test queue with 8 nodes of data.");
 
         // add static values to queue
         testQ.enqueue(1);
@@ -42,46 +42,60 @@ public class Q2 {
         testQ.enqueue(7);
         testQ.enqueue(8);
 
-        System.out.println("Swapping values in queue...\n");
+        System.out.println("Swapping values in queue...");
 
         // swap and print static values in queue
         System.out.print("Initial Queue:  ");
         testQ.printQueue();
-        System.out.print("Swapping 4 & 8: ");
-        testQ.swap(4, 8);
+        System.out.print("Swapping 1 & 8: ");
+        testQ.swap(1, 8);
         testQ.printQueue();
-        System.out.print("Swapping 1 & 6: ");
-        testQ.swap(1, 6);
+        System.out.print("Swapping 4 & 6: ");
+        testQ.swap(4, 6);
         testQ.printQueue();
         System.out.print("Swapping 5 & 2: ");
         testQ.swap(5, 2);
         testQ.printQueue();
+        System.out.println();
     }
 
     public static void DLLDemo() {
 
         DLLQueue testQ = new DLLQueue();
+
+        System.out.println("DLL Demo:");
+        System.out.println("Creating test queue with 8 nodes of data.");
+
         testQ.add(5);
         testQ.add(10);
+        testQ.add(15);
         testQ.add(20);
+        testQ.add(25);
         testQ.add(30);
-        testQ.add(50);
+        testQ.add(35);
+        testQ.add(40);
 
+        System.out.print("Current Queue State:................. ");
         testQ.printQueue();
 
-        testQ.swap(3);
-        testQ.printQueue();
-        testQ.swap(2);
-        testQ.printQueue();
-        testQ.swap(1);
-        testQ.printQueue();
-        testQ.swap(3);
-
-
-        testQ.printQueue();
-
-        testQ.printQueue();
-
-
+        try {
+            System.out.print("Swapping Adjacent Value with index 0: ");
+            testQ.swap(0);
+            testQ.printQueue();
+            System.out.print("Swapping Adjacent Value with index 2: ");
+            testQ.swap(2);
+            testQ.printQueue();
+            System.out.print("Swapping Adjacent Value with index 4: ");
+            testQ.swap(4);
+            testQ.printQueue();
+            System.out.print("Swapping Adjacent Value with index 6: ");
+            testQ.swap(6);
+            testQ.printQueue();
+            System.out.print("Swapping Adjacent Value with index 7: ");
+            testQ.swap(7);
+            testQ.printQueue();
+        } catch (IllegalArgumentException e) {
+            System.out.println("ERROR: You Have Input An Index Value That Cannot Be Swapped!");
+        }
     }
 }
